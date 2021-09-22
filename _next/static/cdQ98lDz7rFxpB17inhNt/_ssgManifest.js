d182f057473662068e32a15cdea50c6214b02400
NaMax66 <max.naid@mail.ru>
2021-09-22 21:58:08 +0500

    change prefix

--- _next/static/cdQ98lDz7rFxpB17inhNt/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fbooks","\u002Ft_shirts","\u002Ftrinkets"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/i6O1HauUfgMqCNahStjA4/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Ftrinkets","\u002Ft_shirts","\u002Fbooks"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
